7c179f052687de111addeea67069ac3464b8d21e
seulgi5041 <crudel44@gmail.com>
2021-09-03 18:12:08 +0900

    0903

---
+++ html5_css3/with/javaS/test.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <script>
+        // 새로운 변수 선언 : var (재사용 시 사용 안 해도 됨.) 
+        var a = 8;
+        var b = 2;
+        var c = 10;
+
+        // 1. 전체 합계를 구하시오
+        // var sum = a + b + c;
+        document.write(a + b + c + "<br />");
+
+        // 2. 전체 평균을 구하시오.
+        // var rest = (a + b + c ) / 3;
+        document.write((a + b + c)/3 + "<br />");
+
+
+        // 3. a+c를 더한 값에서 b로 나눈 값
+        // var na = ( a + c ) / b;
+        document.write((a + c)/b + "<br />");
+
+
+        // 4. c와 b로 나눈 나머지
+        // var rest2 = c % b;
+        document.write(c % b + "<br />");
+
+
+
+        // 5. d라는 변수에 4번의 결과값을 넣어서 a를 곱하시오.
+        // var d = rest2;
+        // var gob = d * a;
+        var d = result * a ;
+        document.write(d + "<br />");
+
+
+
+        // document.write (sum + "<br />");
+        // document.write (rest + "<br />");
+        // document.write (na + "<br />");
+        // document.write (rest2 + "<br />");
+        // document.write (gob + "<br />");
+
+
+
+    </script>
+</body>
+</html>
