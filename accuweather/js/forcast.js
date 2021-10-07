8bdf7dfaabd8b0a2e302cd6f1f56f09b604b3f60
seulgi5041 <crudel44@gmail.com>
2021-10-07 15:03:10 +0900

    1007

--- accuweather/js/forcast.js
+++ accuweather/js/forcast.js
@@ -30,7 +30,7 @@ function getCurrentWeather(city) {
             console.log("code:" + request.status);
             console.log("message:" + request.responseText);
             console.log("error:" + error);
-        },
+        }
 
     });
 
@@ -41,5 +41,29 @@ function getCurrentWeather(city) {
 
 // 현재 날씨의 온도 얻어오기
 function getCurrentTemp(city) {
+    var temp;
+    var openWeatherAPI = url; // city가 계속 붙으므로 url 초기화를 위해 반드시 넣어준다
+
+    $.ajax({
+        type: "GET",
+        url: openWeatherAPI += city,
+        dataType: "json",
+        async: false,  // 결과 데이터를 리턴시키기 위해 동기 방식으로 변경
+
+        success: function(data) {
+            //정상 응답시 처리 작업 
+            temp = Math.floor(data.main.temp); // 소수점 버림;
+        },
+        error: function(request, status, error) {
+            //응답 에러시 처리 작업    
+            console.log("code:" + request.status);
+            console.log("message:" + request.responseText);
+            console.log("error:" + error);
+        }
+
+    });
+
+    return temp;
+
 
 }
--- accuweather/js/main.js
+++ accuweather/js/main.js
@@ -1,3 +1,18 @@
 // 중심이 되는부분. (기능은 각각의 하부 파일에. 여기는 호출해주는곳.)
-var result = getCurrentWeather("incheon");
-console.log(result);
+// var result = getCurrentWeather("incheon");
+// console.log(result);
+
+
+var cityList = ["seoul", "incheon", "gyeonggi-do", "gwangju", "busan", "jeju"];
+
+$(".temp").each(function(i) {
+    $(this).text(getCurrentTemp(cityList[i]) + "℃");
+});
+
+$(".location").on({
+    "click" : function() {
+        var q = $(this).children(".q").attr("id");
+        var redirectURL = "pages/weather_location.html?q=" + q; 
+        location.href = redirectURL;
+    }
+});
